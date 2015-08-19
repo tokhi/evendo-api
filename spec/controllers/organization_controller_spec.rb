19a558afeeda5b30b428390ec1aa1acbc07a8711
tokhi <shafi.tokhi@ilovevideo.tv>
2015-08-19 13:55:34 +0200

    writing test for org controller

--- spec/controllers/organization_controller_spec.rb
+++ spec/controllers/organization_controller_spec.rb
@@ -7,6 +7,9 @@ describe OrganizationsController, :type => :controller  do
     num.times {
       org = Organization.create(name: "CinemaxX#{num}", country: "Germany",
                           city: "Berlin", address: "Potsdamer Straße 5, 10785 Berlin", telephone: "+49 40 80806969", email: "info@cinemaxx.de", category: "cinema")
+      event = Event.create(title: "mad max trailer#{num}", description: "some description here..", ogranization_id: num, event_date: Date.parse("2015-10-13"), location: "Potsdamer Straße 5, 10785 Berlin", seat_nums: 100, price: "15 Eur",
+                           category: "Movie", thumbnail: "http://example.com/xyz1.jpg", link: "http://example.com/xyz1.mp4", rating: "5", custom_fields: {"movieLength"=>"2 hours","PG"=>16,"Genre"=>"Action, thriller"})
+    
     }
     org
   end
@@ -16,5 +19,7 @@ describe OrganizationsController, :type => :controller  do
     get :index
     expect(assigns(:organizations)).to eq([org])
   end
+
+
 
 end
