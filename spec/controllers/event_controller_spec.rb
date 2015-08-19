8c83a3ff2a6f2b5ee2d352d2cd5d75178f6a0a05
tokhi <shafi.tokhi@ilovevideo.tv>
2015-08-19 13:30:34 +0200

    rspec test for index and get events by title

--- spec/controllers/event_controller_spec.rb
+++ spec/controllers/event_controller_spec.rb
@@ -2,4 +2,24 @@ require 'rails_helper'
 
 describe EventsController, :type => :controller  do
 
+  def build(num)
+    event = nil
+    num.times {
+      event = Event.create(title: "mad max trailer#{num}", description: "some description here..", ogranization_id: 1, event_date: Date.parse("2015-10-13"), location: "Potsdamer Straße 5, 10785 Berlin", seat_nums: 100, price: "15 Eur",
+                           category: "Movie", thumbnail: "http://example.com/xyz1.jpg", link: "http://example.com/xyz1.mp4", rating: "5", custom_fields: {"movieLength"=>"2 hours","PG"=>16,"Genre"=>"Action, thriller"})
+    }
+    event
+  end
+  it "assigns @event" do
+    event = build(1)
+    get :index
+    expect(assigns(:events)).to eq([event])
+  end
+
+  it "get event by title" do
+    build(2)
+    get :index, {by_title: "trailer2"}
+    expect(assigns(:events)[0].title).to eq("mad max trailer2")
+  end
+
 end
